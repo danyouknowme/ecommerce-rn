329ae1c48c424ed6b30144e9f2c04dd5e51bc447
Thanathip <dannajaeiei@hotmail.com>
2022-05-02 19:33:15 +0700

    Fixed: change View to TouchableOpacity

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,38 +1,35 @@
 import React from 'react';
-import { View, Text } from 'react-native';
-import tailwind from 'tailwind-rn';
+import { View, Text, TouchableOpacity } from 'react-native';
+import tw from 'twrnc';
 import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-native';
+import { useNavigate } from 'react-router-native';
 
 const Navbar: React.FC = () => {
   const page = useSelector((state: any) => state.app.page as string);
+  const navigate = useNavigate();
+
+  const navigateToAnotherPage = (page: string) => {
+    navigate(`/${page}`);
+  };
 
   return (
-    <View style={tailwind('flex-row justify-between items-center')}>
-      <View
-        style={tailwind(
-          'flex-row items-center bg-red-200 px-3 py-2 rounded-lg'
-        )}
-      >
+    <View style={tw`flex-row justify-between items-center`}>
+      <View style={tw`flex-row items-center bg-red-200 px-3 py-2 rounded-lg`}>
         <FontAwesomeIcon name='home' size={22} color='#b91c1c' />
         {page === 'home' && (
-          <Text style={tailwind('ml-2 text-red-700 font-semibold')}>Home</Text>
+          <Text style={tw`ml-2 text-red-700 font-semibold`}>Home</Text>
         )}
       </View>
-      <Link to='/favourite'>
-        <View>
-          <FontAwesomeIcon name='heart' size={22} color='#bdbdbd' />
-        </View>
-      </Link>
-      <Link to='/cart'>
-        <View>
-          <FontAwesomeIcon name='shopping-cart' size={22} color='#bdbdbd' />
-        </View>
-      </Link>
-      <View>
+      <TouchableOpacity onPress={() => navigateToAnotherPage('favourite')}>
+        <FontAwesomeIcon name='heart' size={22} color='#bdbdbd' />
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => navigateToAnotherPage('cart')}>
+        <FontAwesomeIcon name='shopping-cart' size={22} color='#bdbdbd' />
+      </TouchableOpacity>
+      <TouchableOpacity>
         <FontAwesomeIcon name='user' size={22} color='#bdbdbd' />
-      </View>
+      </TouchableOpacity>
     </View>
   );
 };
