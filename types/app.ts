8fce91110b189c8716f0e377567d4c59ea57bf6e
Thanathip <dannajaeiei@hotmail.com>
2022-05-02 21:16:55 +0700

    Added: interface carousel item props

--- types/app.ts
+++ types/app.ts
@@ -1,3 +1,10 @@
 export interface IAppState {
     page: string;
 }
+
+export interface ICarouselItemProps {
+    message: string;
+    imageUrl: string;
+    color: string;
+    lastObject?: boolean;
+}
