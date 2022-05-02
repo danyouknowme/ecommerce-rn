83559429eb994de0176ffd4ebb3f0e9ecedc75f0
Thanathip <dannajaeiei@hotmail.com>
2022-05-03 03:48:17 +0700

    Added: product cart component using in cart page

---
+++ components/ProductInCart.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { View, Image, Text, StyleSheet } from 'react-native';
+import { Spinner } from '../components';
+import { Product } from '../types/app';
+import tw from 'twrnc';
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+});
+
+const ProductInCart: React.FC<{ product: Product }> = ({ product }) => {
+  const [quantity, setQuantity] = useState(1);
+
+  return (
+    <View
+      style={tw`flex-row px-3 py-2 border bg-white border-zinc-200 rounded-xl`}
+    >
+      <View style={tw`w-18 h-28`}>
+        <Image
+          source={{
+            uri: product?.image,
+          }}
+          style={styles.image}
+        />
+      </View>
+      <View style={tw`flex-1 ml-6 py-3 flex-col justify-between`}>
+        <Text numberOfLines={2} style={tw`font-semibold text-lg`}>
+          {product?.title}
+        </Text>
+        <View style={tw`flex-row justify-between items-center`}>
+          <Text style={tw`font-bold text-3xl`}>${'140'}</Text>
+          <Spinner count={quantity} setCount={setQuantity} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default ProductInCart;
--- components/index.ts
+++ components/index.ts
@@ -4,3 +4,4 @@ export { default as Searchbar } from './Searchbar';
 export { default as ProductCard } from './ProductCard';
 export { default as BackButton } from './BackButton';
 export { default as Spinner } from './Spinner';
+export { default as ProductInCart } from './ProductInCart';
