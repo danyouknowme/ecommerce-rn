beeec729b9079fcffcf233f6ec922069c693a843
Thanathip <dannajaeiei@hotmail.com>
2022-05-03 02:51:15 +0700

    Added: function getProductById

--- services/product.ts
+++ services/product.ts
@@ -5,6 +5,11 @@ export const getAllProducts = () => {
   return response;
 };
 
+export const getProductById = (id: string) => {
+  const response = axios.get(`https://fakestoreapi.com/products/${id}`);
+  return response;
+};
+
 export const checkFilteredSearchEngine = (productTitle: string, searchInput: string) => {
   return productTitle.toLowerCase().includes(searchInput.toLowerCase());
 };
