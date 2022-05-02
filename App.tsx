ab3646c225f4d07e43178cdf4c943819d57a3e5b
Thanathip <dannajaeiei@hotmail.com>
2022-05-02 19:37:17 +0700

    Fixed: remove unnecessary provider and tag

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Text } from 'react-native';
 import { NativeRouter, Route, Routes } from 'react-router-native';
 import { Home, Product, Cart } from './pages';
 import { Provider } from 'react-redux';
@@ -7,16 +6,14 @@ import { store } from './app/store';
 
 export default function App() {
   return (
-    <React.StrictMode>
-      <Provider store={store}>
-        <NativeRouter>
-          <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/product/:productId' element={<Product />} />
-            <Route path='/cart' element={<Cart />} />
-          </Routes>
-        </NativeRouter>
-      </Provider>
-    </React.StrictMode>
+    <Provider store={store}>
+      <NativeRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/product/:productId' element={<Product />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </NativeRouter>
+    </Provider>
   );
 }
