c5f38c10451690d46cb2267b068c06a405d57220
Thanathip <dannajaeiei@hotmail.com>
2022-05-02 23:57:22 +0700

    Added: function to get products data and filter data

---
+++ services/product.ts
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const getAllProducts = () => {
+  const response = axios.get('https://fakestoreapi.com/products');
+  return response;
+};
+
+export const checkFilteredSearchEngine = (productTitle: string, searchInput: string) => {
+  return productTitle.toLowerCase().includes(searchInput.toLowerCase());
+};
