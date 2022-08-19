e7dbe3d841c2b198d228e3b55add1a06a25e80aa
danyouknowme <thanathip.suw@gmail.com>
2022-08-19 18:02:44 +0700

    fixed(button): export home button component

--- components/index.ts
+++ components/index.ts
@@ -1,7 +1,8 @@
-export { default as Navbar } from './Navbar';
-export { default as Carousel } from './Carousel';
-export { default as Searchbar } from './Searchbar';
-export { default as ProductCard } from './ProductCard';
-export { default as BackButton } from './BackButton';
-export { default as Spinner } from './Spinner';
-export { default as ProductInCart } from './ProductInCart';
+export { default as Navbar } from "./Navbar";
+export { default as Carousel } from "./Carousel";
+export { default as Searchbar } from "./Searchbar";
+export { default as ProductCard } from "./ProductCard";
+export { default as BackButton } from "./BackButton";
+export { default as Spinner } from "./Spinner";
+export { default as ProductInCart } from "./ProductInCart";
+export { default as HomeButton } from "./HomeButton";
