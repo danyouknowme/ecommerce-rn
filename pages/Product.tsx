5ca28f0813a2ce6189d621b7736918092402d2ff
danyouknowme <thanathip.suw@gmail.com>
2022-08-19 18:48:59 +0700

    fixed(auth): verify user is logged in before add product to cart

--- pages/Product.tsx
+++ pages/Product.tsx
@@ -1,27 +1,20 @@
-import React, { useState, useEffect } from 'react';
-import {
-  View,
-  Text,
-  SafeAreaView,
-  Image,
-  StyleSheet,
-  TouchableOpacity,
-} from 'react-native';
-import { useParams } from 'react-router-native';
-import { getProductById } from '../services/product';
-import { Product } from '../types/app';
-import { BackButton } from '../components';
-import tw from 'twrnc';
-import { useSelector, useDispatch } from 'react-redux';
-import { setCarts } from '../app/appSlice';
-import { ICart } from '../types/app';
+import React, { useState, useEffect } from "react";
+import { View, Text, SafeAreaView, Image, StyleSheet, TouchableOpacity } from "react-native";
+import { useNavigate, useParams } from "react-router-native";
+import { getProductById } from "../services/product";
+import { Product } from "../types/app";
+import { BackButton } from "../components";
+import tw from "twrnc";
+import { useSelector, useDispatch } from "react-redux";
+import { setCarts } from "../app/appSlice";
+import { ICart } from "../types/app";
 
 const styles = StyleSheet.create({
   image: {
-    position: 'absolute',
-    width: '65%',
-    height: '65%',
-    resizeMode: 'contain',
+    position: "absolute",
+    width: "65%",
+    height: "65%",
+    resizeMode: "contain",
     top: 85,
     left: 65,
   },
@@ -31,7 +24,9 @@ export default function Cart() {
   const { productId } = useParams();
   const [product, setProduct] = useState<Product>();
   const carts = useSelector((state: any) => state.app.carts as ICart[]);
+  const authUser = useSelector((state: any) => state.app.authUser);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (productId) {
@@ -42,6 +37,9 @@ export default function Cart() {
   }, []);
 
   const addProductToCart = () => {
+    if (!authUser) {
+      navigate("/login");
+    }
     dispatch(setCarts([...carts, { product: product, quantity: 1 }]));
   };
 
@@ -68,17 +66,13 @@ export default function Cart() {
         </View>
         <View style={tw`flex-col mt-4`}>
           <Text style={tw`font-semibold text-xl`}>About</Text>
-          <Text style={tw`mt-2`}>
-            {product?.description}
-          </Text>
+          <Text style={tw`mt-2`}>{product?.description}</Text>
         </View>
         <TouchableOpacity
           style={tw`mt-6 w-full bg-red-400 items-center rounded-2xl`}
           onPress={addProductToCart}
         >
-          <Text style={tw`py-3 text-lg font-semibold text-white`}>
-            Add to cart
-          </Text>
+          <Text style={tw`py-3 text-lg font-semibold text-white`}>Add to cart</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
