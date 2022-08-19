a4bbfd7f7e5fd8e81e3be232a9ae0775c70eb1c1
danyouknowme <thanathip.suw@gmail.com>
2022-08-19 18:02:26 +0700

    feat(button): create home button component

---
+++ components/HomeButton.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { useNavigate } from "react-router-native";
+import { TouchableOpacity } from "react-native";
+import FontAwesome from "react-native-vector-icons/FontAwesome";
+import tw from "twrnc";
+
+const HomeButton: React.FC = () => {
+  const navigate = useNavigate();
+  return (
+    <TouchableOpacity style={tw`bg-zinc-300 rounded`} onPress={() => navigate("/")}>
+      <FontAwesome name="home" size={36} color="#141414" style={tw`py px-1`} />
+    </TouchableOpacity>
+  );
+};
+
+export default HomeButton;
