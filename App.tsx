1d7763304da9f5e1a9e974e50931f5b5f983f83a
danyouknowme <thanathip.suw@gmail.com>
2022-08-19 18:50:42 +0700

    fixed(auth): verify user logged in on each page

--- App.tsx
+++ App.tsx
@@ -1,22 +1,30 @@
 import React from "react";
-import { NativeRouter, Route, Routes } from "react-router-native";
+import { NativeRouter, Navigate, Route, Routes } from "react-router-native";
 import { Home, Product, Cart, Auth, Login, Register } from "./pages";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { store } from "./app/store";
 
 export default function App() {
   return (
     <Provider store={store}>
       <NativeRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/product/:productId" element={<Product />} />
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Router />
       </NativeRouter>
     </Provider>
   );
 }
+
+const Router = () => {
+  const authUser = useSelector((state: any) => state.app.authUser);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/product/:productId" element={<Product />} />
+      <Route path="/cart" element={authUser ? <Cart /> : <Navigate to="/login" />} />
+      <Route path="/auth" element={<Auth />} />
+      <Route path="/register" element={!authUser ? <Register /> : <Navigate to="/" />} />
+      <Route path="/login" element={!authUser ? <Login /> : <Navigate to="/" />} />
+    </Routes>
+  );
+};
