338646fd007d2a1792875e8763dfb81ce0143a1e
danyouknowme <thanathip.suw@gmail.com>
2022-08-19 17:44:01 +0700

    feat(auth): added all auth pages

--- App.tsx
+++ App.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import { NativeRouter, Route, Routes } from 'react-router-native';
-import { Home, Product, Cart } from './pages';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
+import React from "react";
+import { NativeRouter, Route, Routes } from "react-router-native";
+import { Home, Product, Cart, Auth, Login, Register } from "./pages";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 export default function App() {
   return (
     <Provider store={store}>
       <NativeRouter>
         <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/product/:productId' element={<Product />} />
-          <Route path='/cart' element={<Cart />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/product/:productId" element={<Product />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </NativeRouter>
     </Provider>
